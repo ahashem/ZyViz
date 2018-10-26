3096651c821a858cf6bb45cad0bb00c50a924289
Ahmed Hashem <me@ahashem.me>
2018-10-26 22:00:00 +0200

    add basic click event handling
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -7,21 +7,46 @@ import { VictoryPie } from 'victory';
  * Abstract Pie Chart wrapper
  * @param data
  */
-const PieChart = ({ data, ...rest }) => {
+const PieChart = ({ data, onClick, ...rest }) => {
   return (
     <VictoryPie
       data={data}
       {...rest}
+      events={[{
+        target: 'data',
+        eventHandlers: {
+          onClick: (e) => {
+            console.log('event', e);
+            return [
+              {
+                target: 'data',
+                mutation: (props) => {
+                  const fill = props.style && props.style.fill;
+                  return fill === '#c43a31' ? null : { style: { fill: '#c43a31' } };
+                }
+              }, {
+                target: 'labels',
+                mutation: (props) => {
+                  return props.text === 'clicked' ? null : { text: 'clicked' };
+                }
+              }
+            ];
+          }
+        }
+      }]}
     />
   );
 };
 
 PieChart.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({}))
+  data: PropTypes.arrayOf(PropTypes.shape({})),
+  onClick: PropTypes.func,
 };
 
 PieChart.defaultProps = {
   data: [],
+  onClick: () => {
+  },
 };
 
 export default PieChart;
