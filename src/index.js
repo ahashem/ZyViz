5b4fd72af269ce04c58beec43cc046f62d1c28ca
Ahmed-Hashem <ahashem@users.noreply.github.com>
2018-10-26 17:06:05 +0200

    prepare for offline mode and add branding (#6)
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from 'speedux';
 
 import App from './App/App';
 import * as serviceWorker from './utils/serviceWorker';
-
+// Global Style files
 import 'antd/dist/antd.css';
 import './styles/index.scss';
 
@@ -18,7 +18,9 @@ const AppWrapper = (
 
 render(AppWrapper, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Enable service workers in Production only to support offline Mode and load faster.
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
