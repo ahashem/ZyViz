8ad2c962d8367dba06f2934d1a258dbe93c9641f
Ahmed Hashem <me@ahashem.me>
2018-10-26 16:26:04 +0200

    update snapshot tests and codecov config
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/views/CrossVisualizer/OrdersCountPie/__tests__/OrdersCountPie.test.js
+++ src/views/CrossVisualizer/OrdersCountPie/__tests__/OrdersCountPie.test.js
@@ -4,27 +4,9 @@ import { shallow } from 'enzyme';
 import OrdersCountPie from '../OrdersCountPie';
 import crossfilter from '../../../../components/crossfilter/crossfilter';
 
+import mockOrders from '../../../../utils/test-utils/mock-test-orders';
+
 describe('<OrdersCountPie /> Component', () => {
-  const mockOrders = [
-    {
-      'userid': '94a16117-9db3-4f25-abdf-1c3ef6578097',
-      'orderid': 36,
-      'orderAmount': '$51.89',
-      'orderdate': '2015-03-16T19:46:12+00:00',
-      'paymentMethod': 'Cash',
-      'branch': 'Branch D',
-      'deliveryArea': 'Mirqab'
-    },
-    {
-      'userid': '0aed2c89-8d05-4a0c-b5dd-35c24829020e',
-      'orderid': 37,
-      'orderAmount': '$22.77',
-      'orderdate': '2015-11-06T12:33:56+00:00',
-      'paymentMethod': 'KNET',
-      'branch': 'Branch A',
-      'deliveryArea': 'Funaitees'
-    },
-  ];
 
   const mockOrdersCrossed = crossfilter(mockOrders);
 
--- src/views/CrossVisualizer/__tests__/CrossVisualizer.test.js
+++ src/views/CrossVisualizer/__tests__/CrossVisualizer.test.js
@@ -4,62 +4,49 @@ import configureStore from 'redux-mock-store';
 
 import CrossVisualizer from '../CrossVisualizer';
 
-const mockOrders = [
-  {
-    'userid': '94a16117-9db3-4f25-abdf-1c3ef6578097',
-    'orderid': 36,
-    'orderAmount': '$51.89',
-    'orderdate': '2015-03-16T19:46:12+00:00',
-    'paymentMethod': 'Cash',
-    'branch': 'Branch D',
-    'deliveryArea': 'Mirqab'
-  },
-  {
-    'userid': '0aed2c89-8d05-4a0c-b5dd-35c24829020e',
-    'orderid': 37,
-    'orderAmount': '$22.77',
-    'orderdate': '2015-11-06T12:33:56+00:00',
-    'paymentMethod': 'KNET',
-    'branch': 'Branch A',
-    'deliveryArea': 'Funaitees'
-  },
-];
-
-
-test('should render children and match snapshot', () => {
-  const crossVisualizer = {
-    loading: false,
-    error: false,
-    errorMes: '',
-    ordersData: mockOrders,
-  };
-  const mockStore = configureStore();
-  const store = mockStore({ crossVisualizer });
-
-  const rendered = shallow(<CrossVisualizer store={store}/>);
-
-  expect(rendered).toMatchSnapshot();
-});
+import mockOrders from '../../../utils/test-utils/mock-test-orders';
 
 describe('<CrossVisualizer /> Component', () => {
-  describe('UI during loading', () => {
-    let wrapper;
+  test('should render children and match snapshot', () => {
+    const crossVisualizer = {
+      loading: false,
+      error: false,
+      errorMes: '',
+      ordersData: mockOrders,
+    };
+    const mockStore = configureStore();
+    const store = mockStore({ crossVisualizer });
 
-    beforeAll(() => {
-      const crossVisualizer = {
-        loading: true,
-      };
-      const mockStore = configureStore();
-      const store = mockStore({ crossVisualizer });
+    const rendered = shallow(<CrossVisualizer store={store}/>);
 
-      wrapper = mount(<CrossVisualizer store={store}/>);
-    });
+    expect(rendered).toMatchSnapshot();
+  });
 
-    test('it should render loading spinner while loading', () => {
-      const spinner = wrapper.find('[data-test="loading"]');
-      expect(spinner.exists()).toBe(true);
+  describe('<CrossVisualizer /> Component UI', () => {
+    describe('UI during loading', () => {
+      let wrapper;
+
+      beforeAll(() => {
+        const crossVisualizer = {
+          loading: true,
+        };
+        const mockStore = configureStore();
+        const store = mockStore({ crossVisualizer });
+
+        wrapper = mount(<CrossVisualizer store={store}/>);
+      });
+
+      test('it should render loading spinner while loading', () => {
+        const spinner = wrapper.find('[data-test="loading"]');
+        expect(spinner.exists()).toBe(true);
+      });
+
+      test('it should not render other elements', () => {
+        const spinner = wrapper.find('[data-test="loading"]');
+        expect(spinner.exists()).toBe(true);
+      });
+
     });
 
   });
-
 });
