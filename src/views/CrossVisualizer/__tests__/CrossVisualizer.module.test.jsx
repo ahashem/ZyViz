47ef77a3e2f836d99b4b04249f1de4a88a603cfc
Ahmed Hashem <me@ahashem.me>
2018-10-27 23:53:22 +0200

    update tests
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/views/CrossVisualizer/__tests__/CrossVisualizer.module.test.jsx
+++ src/views/CrossVisualizer/__tests__/CrossVisualizer.module.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import module from '../CrossVisualizer.module';
 import { OrdersAPI } from '../../../utils/API';
 import mockOrders from '../../../utils/test-utils/mock-test-orders';
+import { crossFilterMock, dimensionsMock } from '../../../utils/test-utils';
 
 jest.mock('../../../utils/API');
 
@@ -9,65 +10,90 @@ jest.mock('../../../utils/API');
 beforeAll(() => {
   const mockStore = {
     getState: () => ({
-      CrossVisualizer: {},
+      crossVisualizer: {
+        orders: crossFilterMock,
+        dimensions: dimensionsMock
+      },
     }),
   };
 
   module.config({
-    name: 'CrossVisualizer',
+    name: 'crossVisualizer',
     store: mockStore,
   });
 });
 describe('<CrossVisualizer /> Dashboard Module', () => {
-  let generator;
-  beforeEach(() => {
-    OrdersAPI.getOrders.mockClear();
-    generator = module.actions.getOrdersData();
-  });
-
-  test('it should loading before getting orders', () => {
-    expect(generator.next().value).toEqual({
-      loading: true,
+  describe('Get Orders API', () => {
+    let generator;
+    beforeEach(() => {
+      OrdersAPI.getOrders.mockClear();
+      generator = module.actions.getOrdersData();
     });
-  });
 
-  test('it should return all orders and save it to the state', async () => {
-    OrdersAPI.getOrders.mockReturnValue(new Promise((resolve) => {
-      resolve(mockOrders);
-    }));
-    generator.next();
-    const allOrders = await generator.next().value;
-    expect(allOrders).toEqual(mockOrders);
-  });
+    test('it should loading before getting orders', () => {
+      expect(generator.next().value).toEqual({
+        loading: true,
+      });
+    });
 
-  test('it should handle returning error from API', async () => {
-    generator.next();
+    test('it should return all orders and save it to the state', async () => {
+      OrdersAPI.getOrders.mockReturnValue(new Promise((resolve) => {
+        resolve(mockOrders);
+      }));
+      generator.next();
+      const allOrders = await generator.next().value;
+      expect(allOrders).toEqual(mockOrders);
+    });
 
-    OrdersAPI.getOrders.mockReturnValue(new Promise((resolve, reject) => {
-      const error = {
-        errorCode: 500,
+    test('it should handle returning error from API', async () => {
+      generator.next();
+
+      OrdersAPI.getOrders.mockReturnValue(new Promise((resolve, reject) => {
+        const error = {
+          errorCode: 500,
+          errorMsg: 'Sorry, an error has occurred',
+        };
+        reject(new Error(JSON.stringify(error)));
+      }));
+
+      const allOrders = await generator.next().value;
+
+      expect(generator.next(allOrders).value).toEqual({
+        error: true,
         errorMsg: 'Sorry, an error has occurred',
-      };
-      reject(new Error(JSON.stringify(error)));
-    }));
+        loading: false,
+      });
+    });
 
-    const allOrders = await generator.next().value;
+    test('it should finish loading after getting orders', async () => {
+      generator.next();
+      OrdersAPI.getOrders.mockReturnValue(new Promise((resolve) => {
+        resolve(mockOrders);
+      }));
 
-    expect(generator.next(allOrders).value).toEqual({
-      error: true,
-      errorMsg: 'Sorry, an error has occurred',
-      loading: false,
+      const ordersData = await generator.next().value;
+
+      expect(generator.next(ordersData).value).toHaveProperty('loading', false);
     });
   });
 
-  test('it should finish loading after getting orders', async () => {
-    generator.next();
-    OrdersAPI.getOrders.mockReturnValue(new Promise((resolve) => {
-      resolve(mockOrders);
-    }));
+  describe('Update Dimensions on Filter', () => {
+    let generator;
+    beforeEach(() => {
+      generator = module.actions.updateFilters;
+    });
 
-    const ordersData = await generator.next().value;
+    test('it should not update store if no dimension were provided', async () => {
+      //dimension, filterKey, clear
+      // generator();
+      expect(generator().next().value).toBeFalsy();
+    });
 
-    expect(generator.next(ordersData).value).toHaveProperty('loading', false);
+    test('it should update store if no dimension were provided', async () => {
+      const dimension = 'ordersByPaymentMethod';
+      const filterKey = 'Cash';
+      expect(generator(dimension, filterKey).next().value).toHaveProperty('dimensions');
+    });
   });
+
 });
