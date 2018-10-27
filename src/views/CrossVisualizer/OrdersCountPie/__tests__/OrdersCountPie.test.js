d88c83cbab91d1beb7f6eaad406d0ee45a25e981
Ahmed Hashem <me@ahashem.me>
2018-10-27 21:32:04 +0200

    update tests
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/views/CrossVisualizer/OrdersCountPie/__tests__/OrdersCountPie.test.js
+++ src/views/CrossVisualizer/OrdersCountPie/__tests__/OrdersCountPie.test.js
@@ -3,16 +3,23 @@ import { shallow } from 'enzyme';
 
 import OrdersCountPie from '../OrdersCountPie';
 import crossfilter from '../../../../components/crossfilter/crossfilter';
-
 import mockOrders from '../../../../utils/test-utils/mock-test-orders';
+import { GenerateDimensions, NormalizeData } from '../../visualizer-data-structure';
+
+const mockOrdersCrossed = crossfilter(NormalizeData(mockOrders));
+const mockOrdersDimensions = GenerateDimensions(mockOrdersCrossed);
 
 describe('<OrdersCountPie /> Component', () => {
 
-  const mockOrdersCrossed = crossfilter(mockOrders);
-
   test('should render children and match snapshot', () => {
+    const dimensions = mockOrdersDimensions;
+    const onFilter = jest.fn();
 
-    const rendered = shallow(<OrdersCountPie orders={mockOrdersCrossed}/>);
+    const rendered = shallow(<OrdersCountPie
+      orders={mockOrdersCrossed}
+      dimensions={dimensions}
+      onFilter={onFilter}
+    />);
 
     expect(rendered).toMatchSnapshot();
   });
