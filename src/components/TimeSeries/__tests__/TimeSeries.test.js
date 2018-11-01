efd7ce187d76accd4c7b39d16a6193e004a013f6
Ahmed Hashem <me@ahashem.me>
2018-11-01 05:36:46 +0200

    fix dates in TimeSeries snapshots
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/components/TimeSeries/__tests__/TimeSeries.test.js
+++ src/components/TimeSeries/__tests__/TimeSeries.test.js
@@ -6,14 +6,14 @@ import TimeSeries from '../TimeSeries';
 describe('<TimeSeries /> component', () => {
   it('should render with simple data', () => {
     const mockTimeSeriesData = [
-      { x: new Date(2019, 1, 1), y: 125 },
-      { x: new Date(2018, 1, 1), y: 257 },
-      { x: new Date(2015, 1, 1), y: 345 },
-      { x: new Date(2015, 1, 1), y: 515 },
-      { x: new Date(2011, 1, 1), y: 132 },
-      { x: new Date(2010, 1, 1), y: 305 },
-      { x: new Date(2011, 1, 1), y: 270 },
-      { x: new Date(2015, 1, 1), y: 470 }
+      { x: new Date(Date.UTC(2019, 1, 1)), y: 125 },
+      { x: new Date(Date.UTC(2018, 1, 1)), y: 257 },
+      { x: new Date(Date.UTC(2015, 1, 1)), y: 345 },
+      { x: new Date(Date.UTC(2015, 1, 1)), y: 515 },
+      { x: new Date(Date.UTC(2011, 1, 1)), y: 132 },
+      { x: new Date(Date.UTC(2010, 1, 1)), y: 305 },
+      { x: new Date(Date.UTC(2011, 1, 1)), y: 270 },
+      { x: new Date(Date.UTC(2015, 1, 1)), y: 470 }
     ];
 
     const rendered = shallow(
