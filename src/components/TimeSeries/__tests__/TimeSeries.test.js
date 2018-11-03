d5936bb3d3d42a0cda4683eaa467ed567f197d28
Ahmed-Hashem <ahashem@users.noreply.github.com>
2018-11-03 22:39:10 +0200

    Mock dates (#16)
    * add mock dates,
    zoom Dates in UTC,
    update snapshots with mocked dates,
    update date handling to use date parse
    Signed-off-by: Ahmed Hashem <me@ahashem.me>

--- src/components/TimeSeries/__tests__/TimeSeries.test.js
+++ src/components/TimeSeries/__tests__/TimeSeries.test.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import MockDate from 'mockdate';
 
 import TimeSeries from '../TimeSeries';
 
 describe.skip('<TimeSeries /> component', () => {
-  it('should render with simple data', () => {
+  beforeAll(() => {
+    MockDate.set('11/3/2018');
+    Date.constructor = jest.fn(() => 1541196000000);
+  });
+
+  test('should render with simple data', () => {
     const mockTimeSeriesData = [
-      { x: new Date(Date.UTC(2019, 1, 1)), y: 125 },
-      { x: new Date(Date.UTC(2018, 1, 1)), y: 257 },
-      { x: new Date(Date.UTC(2015, 1, 1)), y: 345 },
-      { x: new Date(Date.UTC(2015, 1, 1)), y: 515 },
-      { x: new Date(Date.UTC(2011, 1, 1)), y: 132 },
-      { x: new Date(Date.UTC(2010, 1, 1)), y: 305 },
-      { x: new Date(Date.UTC(2011, 1, 1)), y: 270 },
-      { x: new Date(Date.UTC(2015, 1, 1)), y: 470 }
+      { x: new Date(Date.parse('2018-03-11T00:00:00+00:00')), y: 125 },
+      { x: new Date(Date.parse('2018-03-11T00:00:00+00:00')), y: 257 },
+      { x: new Date(Date.parse('2018-11-11T00:00:00+00:00')), y: 345 },
+      { x: new Date(Date.parse('2018-07-11T00:00:00+00:00')), y: 515 },
+      { x: new Date(Date.parse('2018-04-11T00:00:00+00:00')), y: 132 },
+      { x: new Date(Date.parse('2014-03-11T00:00:00+00:00')), y: 305 },
+      { x: new Date(Date.parse('2015-03-11T00:00:00+00:00')), y: 270 },
+      { x: new Date(Date.parse('2016-03-11T00:00:00+00:00')), y: 470 }
     ];
 
     const rendered = shallow(
@@ -23,6 +29,10 @@ describe.skip('<TimeSeries /> component', () => {
     );
 
     expect(rendered).toMatchSnapshot();
+  });
+
+  afterAll(() => {
+    MockDate.reset();
   });
 });
 
